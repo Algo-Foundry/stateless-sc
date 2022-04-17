090dc3c29efd36b45d8d1e80b21ce4095a27bee2
Charlie Kong <charlie@undercurrent.tech>
2022-04-17 14:25:17 +0800

    delegated approval

---
+++ README.md
@@ -0,0 +1,1 @@
+# Stateless smart contract assignment
---
+++ scripts/delegated.js
@@ -0,0 +1,8 @@
+const { executeTransaction } = require("@algo-builder/algob");
+const { types } = require("@algo-builder/web");
+
+async function run (runtimeEnv, deployer) {
+    // write your code here
+}
+
+module.exports = { default: run };
